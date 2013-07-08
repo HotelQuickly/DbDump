8296f358dab6cceff69dd9ed62d9a18ecbbdd79a
Jetsada Machom <zinuzoid@gmail.com>
2013-07-08 12:55:30 +0700

    add new config.template.py

---
+++ config.template.py
@@ -0,0 +1,19 @@
+DB_REMOTE_HOST = "localhost"
+DB_REMOTE_NAME = "hqlive"
+DB_REMOTE_USER = "db"
+DB_REMOTE_PASSWORD = "password"
+
+DB_LOCAL_HOST = "localhost"
+DB_LOCAL_NAME = "hqlive"
+DB_LOCAL_USER = "db"
+DB_LOCAL_PASSWORD = "password"
+
+TMP_DIRECTORY = "/home/zinuzoid/workspace/hotelquickly/HQLiveDump/tmp"
+
+TABLE_BLACKLIST = ['contact_2013-06-03', 'hotel_backup_20130624', \
+                   'log_offer', 'log_visit', 'room_backup_20130618', \
+                   'user_friend', 'wego_room_rate_20130430', \
+                   'wego_room_rate_20130528']
+
+DB_LOCAL_NAME_TEMPORARY = "hqlive_dump"
+
