c820932f667217eb4f90bd910a796a1ea7f98adf
Josef Nevoral <josef.nevoral@gmail.com>
2013-07-12 11:26:04 +0700

    Ignore too big tables for dump

--- config.template.py
+++ config.template.py
@@ -10,7 +10,7 @@ DB_LOCAL_PASSWORD = "password"
 
 TMP_DIRECTORY = "/home/zinuzoid/workspace/hotelquickly/HQLiveDump/tmp"
 
-TABLE_BLACKLIST = ['contact_2013-06-03', 'hotel_backup_20130624', \
+TABLE_BLACKLIST = ['email', 'contact', 'user_friend', 'wego_room_rate', 'contact_2013-06-03', 'hotel_backup_20130624', \
                    'log_offer', 'log_visit', 'room_backup_20130618', \
                    'user_friend', 'wego_room_rate_20130430', \
                    'wego_room_rate_20130528']
