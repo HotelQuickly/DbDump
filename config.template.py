b2c73c89bb32eec5592857d41a4e0b7b89722c3b
Josef Nevoral <josef.nevoral@gmail.com>
2014-01-15 15:10:15 +0700

    Update config for log visit table

--- config.template.py
+++ config.template.py
@@ -1,18 +1,18 @@
-DB_REMOTE_HOST = "localhost"
+DB_REMOTE_HOST = "hqlive-replica-2.cgdruaneoo0o.ap-southeast-1.rds.amazonaws.com"
 DB_REMOTE_NAME = "hqlive"
-DB_REMOTE_USER = "db"
-DB_REMOTE_PASSWORD = "password"
+DB_REMOTE_USER = "hqlive"
+DB_REMOTE_PASSWORD = "Ho7e7Qck"
 
 DB_LOCAL_HOST = "localhost"
-DB_LOCAL_NAME = "hqlive"
-DB_LOCAL_USER = "db"
-DB_LOCAL_PASSWORD = "password"
+DB_LOCAL_NAME = "hotel_quickly"
+DB_LOCAL_USER = "root"
+DB_LOCAL_PASSWORD = "kopretina1"
 
-TMP_DIRECTORY = "/home/zinuzoid/workspace/hotelquickly/HQLiveDump/tmp"
+TMP_DIRECTORY = "/Users/lovec/temp/"
 
 TABLE_BLACKLIST = ['hist_log', 'device_session', 'hist_sql', 'email', 'contact', 'user_friend', 'wego_room_rate', \
-                   'log_offer', 'log_visit', 'log_visit_backup20130724', \
-                   'user_friend']
+                   'log_offer', 'log_activity_app', 'log_visit', 'log_visit_201311', 'log_visit_201401', \
+                   'user_friend', 'third_party_prices']
 
 DB_LOCAL_NAME_TEMPORARY = "hqlive_dump"
 
